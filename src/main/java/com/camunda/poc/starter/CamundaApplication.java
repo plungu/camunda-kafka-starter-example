98eb5a38dcd5dcb408e780262474bf410f84081b
Paul Lungu <paul.lungu@camunda.com>
2020-08-18 12:33:47 -0600

    jobs load testing example

--- src/main/java/com/camunda/poc/starter/CamundaApplication.java
+++ src/main/java/com/camunda/poc/starter/CamundaApplication.java
@@ -1,6 +1,7 @@
 package com.camunda.poc.starter;
 
 import com.camunda.poc.starter.usecase.renewal.AppConfigProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(scanBasePackages={"com.camunda.poc.starter","com.camunda.poc.starter.usecase.renewal"})
 @EnableProcessApplication("spring-boot-starter")
 @EnableConfigurationProperties(AppConfigProperties.class)
+@EnableAdminServer
 public class CamundaApplication {
 
 	public static void main(String... args) {
