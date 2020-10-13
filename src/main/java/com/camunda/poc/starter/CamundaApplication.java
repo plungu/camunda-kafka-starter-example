f145e7a575d34748442d6df2133779fc71e2129c
Paul Lungu <paul.lungu@camunda.com>
2020-10-13 14:41:52 -0600

    engine only spring-boot strater

--- src/main/java/com/camunda/poc/starter/CamundaApplication.java
+++ src/main/java/com/camunda/poc/starter/CamundaApplication.java
@@ -1,13 +1,11 @@
 package com.camunda.poc.starter;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication(scanBasePackages={"com.camunda.poc.starter"})
 @EnableProcessApplication("spring-boot-starter")
-@EnableAdminServer
 public class CamundaApplication {
 
 	public static void main(String... args) {
