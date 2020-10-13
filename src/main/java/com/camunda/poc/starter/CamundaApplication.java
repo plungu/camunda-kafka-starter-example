8a7f69722479429407f7196af27ee4998ba991e4
Paul Lungu <paul.lungu@camunda.com>
2020-10-13 14:21:55 -0600

    starter eninge only

--- src/main/java/com/camunda/poc/starter/CamundaApplication.java
+++ src/main/java/com/camunda/poc/starter/CamundaApplication.java
@@ -1,16 +1,12 @@
 package com.camunda.poc.starter;
 
-import com.camunda.poc.starter.usecase.renewal.AppConfigProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages={"com.camunda.poc.starter","com.camunda.poc.starter.usecase.renewal"})
+@SpringBootApplication()
 @EnableProcessApplication("spring-boot-starter")
-@EnableConfigurationProperties(AppConfigProperties.class)
 @EnableAdminServer
 public class CamundaApplication {
 
--- src/main/java/com/camunda/poc/starter/usecase/renewal/repo/CamundaTaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.camunda.poc.starter.usecase.renewal.repo;
-
-
-import com.camunda.poc.starter.usecase.renewal.entity.CamundaTask;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CamundaTaskRepository extends PagingAndSortingRepository<CamundaTask, Long>{}
